aaadc03029f768919fc177a4cab0b4e75bd970ac
Yevhenii <zkhasanovv@gmail.com>
2022-02-11 22:45:34 +0200

    Install prettier. Add scripts to run prettier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,17 +8,9 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { PostPageComponent } from './post-page/post-page.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MainLayoutComponent,
-    HomePageComponent,
-    PostPageComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent, MainLayoutComponent, HomePageComponent, PostPageComponent],
+	imports: [BrowserModule, AppRoutingModule],
+	providers: [],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/post-page/post-page.component.ts
+++ src/app/post-page/post-page.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-post-page',
-  templateUrl: './post-page.component.html',
-  styleUrls: ['./post-page.component.scss']
+	selector: 'app-post-page',
+	templateUrl: './post-page.component.html',
+	styleUrls: ['./post-page.component.scss'],
 })
 export class PostPageComponent implements OnInit {
+	constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
