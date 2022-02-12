811586456fe397893c85ba6cdfb1bb393c36eae3
Yevhenii <zkhasanovv@gmail.com>
2022-02-12 19:06:06 +0200

    Create base layout and styles for admin-layout

--- src/app/admin/shared/components/admin-layout/admin-layout.component.ts
+++ src/app/admin/shared/components/admin-layout/admin-layout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-admin-layout',
@@ -6,7 +7,12 @@ import { Component, OnInit } from '@angular/core';
 	styleUrls: ['./admin-layout.component.scss'],
 })
 export class AdminLayoutComponent implements OnInit {
-	constructor() {}
+	constructor(private router: Router) {}
 
 	ngOnInit(): void {}
+
+	logout($event: Event): void {
+		$event.preventDefault();
+		this.router.navigate(['/admin', 'login']);
+	}
 }
